73ba00878f3c8a1097a131ec4ef945277f483013
FullForYou <fullmetal82@gmail.com>
2016-10-06 12:03:43 +0200

    add auth add subscribe add items (get all)

--- REST.js
+++ REST.js
@@ -33,7 +33,7 @@ function getDate(){
 function checkToken(req, res, next) {
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
   if (token) {
-    jwt.verify(token, 'SECRET_TOKEN', function(err, decoded) {
+    jwt.verify(token, secret(), function(err, decoded) {
       if (err) {
         res.status(401).json({"Error" : true, "code": 401, "Message" : "Failed to authenticate token", "Decoded" : decoded});
       } else {
@@ -48,7 +48,6 @@ function checkToken(req, res, next) {
   }
 }
 
-
 REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
     var self = this;
 
@@ -56,6 +55,133 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
     router.get("/",function(req,res){
       res.status(200).json({"Message" : "I am alive !"});
     });
+
+
+    /* TRY ZONE */
+    router.get("/test", function(req,res,next) {
+      //SELECT idParis, coteEquipe1, coteEquipe2, equipe1Paris, equipe2Paris FROM paris;
+      var query = "SELECT * FROM ??"
+      var table = ["user"];
+      query = mysql.format(query,table);
+      connection.query(query,function(err,rows){
+          if (err) {
+              res.status(500).json({"Error" : true, "Message" : "Error executing MySQL query"});
+          } else {
+              res.status(200).json({"Error" : false, "code" : 200, "Message" : "Success", "Result" : rows});
+          }
+      });
+    });
+    /* END TRY ZONE */
+
+
+
+
+
+
+
+
+    router.post("/authenticate", function(req,res) {
+    // SELECT count(*) as auth FROM user WHERE user_mail = 'toto@toto.com' AND user_pwd = 'toto'
+    //        UNION SELECT user_id FROM user WHERE user_mail = 'toto@toto.com' AND user_pwd = 'toto'
+    // return 2 rows : count AND idUtilisateur
+    var query = "SELECT count(*) as auth FROM ?? WHERE ?? = ? AND ?? = ? UNION SELECT ?? FROM ?? WHERE ?? = ? AND ?? = ?";
+    var table = ["user", "user_mail", req.body.email, "user_pwd", req.body.pwd, "user_id", "user", "user_mail", req.body.email, "user_pwd", req.body.pwd];
+    query = mysql.format(query,table);
+    console.log(query);
+    connection.query(query,function(err,rows){
+      if (err) {
+          res.status(500).json({"Error" : true, "Message" : "Error executing MySQL query"});
+      } else {
+        if(rows[0]['auth'] == 1){
+          _idUser = rows[1]['auth'];
+          var token = jwt.sign({ _id: _idUser, _email: req.body.email}, secret(), {
+            expiresIn: 10080 // (1440 (1 day)) expires in 7 days
+          });
+          res.status(200).json({ "Error": false, "code" : 200, "Message" : 'Welcome !', "token" : token, "Result" : rows});
+        } else {
+          res.status(403).json({"Error" : true, "code" : 403, "Message" : "Authentication failed, wrong username or password" });
+        }
+      }
+    });
+  });
+
+  router.post("/subscribe", function(req, res) {
+    if (!req.body.email || !req.body.pwd || !req.body.firstName || !req.body.lastName || !req.body.age || !req.body.surname) {
+      res.status(400).json({"Error" : true, "code": 400 ,"Message" : "email or pwd required" });
+    }
+    try {
+          var CheckIfAlreadyRegister = new Promise((resolve, reject) => {
+            // SELECT * FROM `esgi-hackathon`.user WHERE user_mail = 'toto@toto.com'
+            var query = "SELECT * FROM ?? WHERE ?? = ?"
+            var table = ["user", "user_mail", req.body.email];
+            query = mysql.format(query,table);
+            console.log(query);
+            connection.query(query,function(err,rows){
+              if (err) {
+                console.log('toto');
+                reject(err);
+                return;
+              } else {
+                console.log('to');
+                console.log(rows[0]);
+                if (rows[0] !== undefined) {
+                  console.log('already in');
+                  res.status(400).json({"Error" : true, "code": 400 ,"Message" : "exist" });
+                  reject(err);
+                  return;
+                } else {
+                  console.log('ok');
+                  resolve(rows);
+                }
+              }
+            });
+          });
+
+          CheckIfAlreadyRegister.then((rows) => {
+            var currentDate = new Date().toLocaleString();
+            console.log(currentDate);
+            if (!req.body.avatar) {
+              req.body.avatar = '';
+            }
+              // INSERT INTO table (user_firstName, user_lastName, user_pwd, user_avatar, user_age, user_mail) VALUES ('to', 'c', 't')
+              // user_firstName, user_lastName, user_pwd, user_avatar, user_age, user_mail
+              var query = "INSERT INTO ?? (??, ??, ??, ??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+              var table = ["user", "user_firstName", "user_lastName", "user_subDate", "user_pwd", "user_avatar", "user_age", "user_mail", "user_surname", "user_isPremium", req.body.firstName, req.body.lastName, currentDate, req.body.pwd, req.body.avatar, req.body.age, req.body.email, req.body.surname, 0];
+              query = mysql.format(query,table);
+              console.log(query);
+
+              connection.query(query,function(err,rows){
+                  if (err) {
+                      res.status(500).json({"Error" : true, "Message" : "Error executing MySQL query"});
+                  } else {
+                      res.status(200).json({"Error" : false, "code" : 200, "Message" : "Success", "Result" : rows});
+                  }
+              });
+          });
+    } catch (errr) {
+      res.status(500).json({"Error" : true, "Message" : "Error" });
+    }
+  });
+
+
+
+
+
+  /* ITEMS AND BUY */
+  router.get("/items", function(req,res,next) {
+    //SELECT idParis, coteEquipe1, coteEquipe2, equipe1Paris, equipe2Paris FROM paris;
+    var query = "SELECT * FROM ??"
+    var table = ["item"];
+    query = mysql.format(query,table);
+    connection.query(query,function(err,rows){
+        if (err) {
+            res.status(500).json({"Error" : true, "Message" : "Error executing MySQL query"});
+        } else {
+            res.status(200).json({"Error" : false, "code" : 200, "Message" : "Success", "Result" : rows});
+        }
+    });
+  });
+
 }
 
 module.exports = REST_ROUTER;
