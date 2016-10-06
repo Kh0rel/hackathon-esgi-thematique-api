f2e41fc20cfacec2f1d7bb1287df196728cd67c6
FullForYou <fullmetal82@gmail.com>
2016-10-07 01:19:58 +0200

    no test

--- test/api.specs.js
+++ test/api.specs.js
@@ -1,17 +1,15 @@
 var request = require('supertest');
 var api = require('../Server');
 
-
 describe('TESTING API HACKATHON', function() {
 
   describe('GET /api_esgi_hackathon/', function() {
     /*
     it('should return welcome message', function() {
-      // console.log(api.route);
-      return request(api)
-      .get('/api_esgi_hackathon/')
-      .send()
-      .expect(200);
+       return request(api)
+       .get("/api_esgi_hackathon/")
+       .send()
+       .expect("200");
     });
     */
   });
