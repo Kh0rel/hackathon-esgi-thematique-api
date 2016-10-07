081822d50099a010b82fe9e99ca17f56c82d1d10
FullForYou <fullmetal82@gmail.com>
2016-10-07 03:00:13 +0200

    fix issue heroic ?

--- Server.js
+++ Server.js
@@ -53,10 +53,10 @@ REST.prototype.configureExpress = function(connection) {
 }
 
 REST.prototype.startServer = function() {
-  var port = process.env.PORT || 3000
-      app.listen(port,function(){
-          console.log("Welcome \nhttp://localhost:3000/api_esgi_hackathon/");
-      });
+  var port = process.env.PORT || 3000;
+  app.listen(port,function(){
+      console.log("Welcome \nhttp://localhost:3000/api_esgi_hackathon/");
+  });
 
 }
 
